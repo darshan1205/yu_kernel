8a9390e3cf0a12362df320a03463e16459e91e25
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2016-06-19 15:23:20 +0000

    msm8916: Add Intel Governor
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -450,6 +450,15 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PMC)
+extern struct cpufreq_governor cpufreq_gov_pmc;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pmc)
+#elif defined(CONFIG_CPU_FREQ_GOV_INTEL)
+extern struct cpufreq_governor cpufreq_gov_intel;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intel)
 #endif
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
